7a56cfb016722ef70e894f7415b7763bcb78b798
aspired2001 <100749624+aspired2001@users.noreply.github.com>
2022-07-02 20:34:39 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+import react from "react";
+import reactDOM from "react-dom";
+const date = new Date();
+const currentTime = date.getHours();
+let greeting;
+const customStyle = {
+  color: ""
+};
+if (currentTime < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (currentTime < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "green";
+} else {
+  greeting = "Good Night";
+  customStyle.color = "blue";
+}
+reactDOM.render(
+  <h1 className="heading" style={customStyle}>
+    {greeting}
+  </h1>,
+  document.getElementById("root")
+);
